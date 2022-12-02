386d528d578e53cb2baf378a0ac36aa6d7534ff7
Andrei2381283 <87148148+Andrei2381283@users.noreply.github.com>
2022-12-02 09:22:02 +0300

    Update mobileMenu.js

--- components/mobileMenu/mobileMenu.js
+++ components/mobileMenu/mobileMenu.js
@@ -2,8 +2,6 @@ import { useTranslation } from 'next-i18next';
 import React, { useState } from 'react';
 import { ThemeContext, themes } from '../../theme/theme';
 import GreenBtn from '../greenBtn/greenBtn';
-import ChangeLang from '../header/changeLang/changeLang';
-import { HeaderThemeContainer, HeaderThemePoint } from '../header/styles';
 import LightBtn from '../lightBtn/lightBtn';
 import { MobileMenuContainer, MobileMenuDiv, MobileMenuLink } from './styles';
 
@@ -12,7 +10,6 @@ const MobileMenu = ({ isMobileMenuVisible, setMobileMenuVisible }) => {
     const { t, i18n } = useTranslation("header");
 
     const [menuVisible, setMenuVisible] = useState(true);
-    console.log(menuVisible, isMobileMenuVisible);
 
     const hide = () => {
         if (!menuVisible) return;
@@ -33,11 +30,6 @@ const MobileMenu = ({ isMobileMenuVisible, setMobileMenuVisible }) => {
                     <MobileMenuLink href="/funds">{t("withdraw")}</MobileMenuLink>
                     <GreenBtn style={{ width: "auto", marginTop: "20px" }} small={true}>{t("reg", { ns: "common" })}</GreenBtn>
                     <LightBtn style={{ width: "auto", marginTop: "10px" }} >{t("login", { ns: "common" })}</LightBtn>
-                    {/* <HeaderThemeContainer style={{margin: "30px auto 0", fontSize: "20px"}} onClick={() => changeTheme(theme == themes.light ? themes.dark : themes.light)}>
-                        <span>{theme.name}</span>
-                        <HeaderThemePoint color={theme == themes.dark ? "255, 255, 255" : "22, 35, 41"} />
-                    </HeaderThemeContainer>
-                    <ChangeLang style={{margin: "30px auto 0", fontSize: "20px"}} /> */}
                 </MobileMenuDiv>
             }
         </ThemeContext.Consumer>
