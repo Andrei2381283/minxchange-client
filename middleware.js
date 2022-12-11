e5583ac11441ffac3accbe068809dccc27d525be
Andrei2381283 <87148148+Andrei2381283@users.noreply.github.com>
2022-12-11 09:48:56 +0300

    Update

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
-import cookie from "cookie";
 import { NextResponse, NextRequest } from 'next/server'
 import { verifyToken } from '../utils/api';
 
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl;
     if (pathname == '/admin') {
-        const isVerify = await verifyToken(cookie.parse(req.headers.cookie || "").token || "");
+        const isVerify = await verifyToken(req.cookies.get('token')?.value || "");
         if(!isVerify) return NextResponse.redirect('/');
     }
     return NextResponse.next();
